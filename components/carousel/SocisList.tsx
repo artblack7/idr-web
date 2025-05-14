040de1921a0569d096c4f375fad7ad776da070f4
artblack <stormeverrise@gmail.com>
2025-05-14 19:53:04 +0200

    load eager

--- components/carousel/SocisList.tsx
+++ components/carousel/SocisList.tsx
@@ -8,49 +8,49 @@ const SocisList = () => {
 
         <div className="Socis">
           <a href="https://curtidosbadia.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/01.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/01.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           <a href="https://combalia.es" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/02.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/02.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           <a href="https://fontpalomas.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/03.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/03.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           <a href="https://splenda.es" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/04.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/04.png' width={570} height={300} loading="eager"/>
           </a>
         </div>              <div className="Socis">
           <a href="https://fontanellasymarti.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/05.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/05.png' width={570} height={300} loading="eager"/>
           </a>
         </div>              <div className="Socis">
           <a href="https://despell.es" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/06.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/06.png' width={570} height={300} loading="eager"/>
           </a>
         </div>              <div className="Socis">
           <a href="https://miretycia.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/09.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/09.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           <a href="https://ppp.cat" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/08.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/08.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           <a href="https://desten.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/07.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/07.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           <a href="https://lancina.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/10.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/10.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
@@ -60,78 +60,78 @@ const SocisList = () => {
         </div>
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/12.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/12.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           <a href="https://curtits-aqualata.es" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/16.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/16.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           <a href="https://curtidosmuntadas.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/14.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/14.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/21.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/21.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/23.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/23.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/15.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/15.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           <a href="https://comercialgodo.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/22.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/22.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/17.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/17.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/11.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/11.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/18.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/18.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/26.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/26.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           {/* <a href="https://escorpion.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/20.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/20.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           {/* <a href="https://escorpion.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/25.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/25.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
 
         <div className="Socis">
           {/* <a href="https://website.com" target="_blank" rel="noopener noreferrer"> */}
-            <Image alt="logo" src='/socis/19.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/19.png' width={570} height={300} loading="eager"/>
           {/* </a> */}
         </div>
         <div className="Socis">
           <a href="https://escorpion.com" target="_blank" rel="noopener noreferrer">
-            <Image alt="logo" src='/socis/24.png' width={570} height={300} />
+            <Image alt="logo" src='/socis/24.png' width={570} height={300} loading="eager"/>
           </a>
         </div>
         
