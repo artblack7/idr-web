743698c07d7e4ac2d93508a1905d56cb24a2d519
artblack <stormeverrise@gmail.com>
2025-07-02 18:57:49 +0200

    change

--- components/carousel/Socis.tsx
+++ components/carousel/Socis.tsx
@@ -24,9 +24,9 @@ const Socis = () => {
         <Image alt="logo" src='/socis/14.png' width={570} height={300} />
         <Image alt="logo" src='/socis/15.png' width={570} height={300} />
         <Image alt="logo" src='/socis/16.png' width={570} height={300} />
+        <Image alt="logo" src='/socis/22.png' width={570} height={300} />  
         <Image alt="logo" src='/socis/21.png' width={570} height={300} />
         <Image alt="logo" src='/socis/17.png' width={570} height={300} />  
-        <Image alt="logo" src='/socis/22.png' width={570} height={300} />  
         <Image alt="logo" src='/socis/18.png' width={570} height={300} />    
         <Image alt="logo" src='/socis/19.png' width={570} height={300} />
         <Image alt="logo" src='/socis/23.png' width={570} height={300} />  
@@ -34,6 +34,7 @@ const Socis = () => {
         <Image alt="logo" src='/socis/25.png' width={570} height={300} />
         <Image alt="logo" src='/socis/24.png' width={570} height={300} />
 
+        <Image alt="logo" src='/socis/01.png' width={570} height={300} />
         <Image alt="logo" src='/socis/02.png' width={570} height={300} />
         <Image alt="logo" src='/socis/03.png' width={570} height={300} />
         <Image alt="logo" src='/socis/04.png' width={570} height={300} />
@@ -50,9 +51,9 @@ const Socis = () => {
         <Image alt="logo" src='/socis/14.png' width={570} height={300} />
         <Image alt="logo" src='/socis/15.png' width={570} height={300} />
         <Image alt="logo" src='/socis/16.png' width={570} height={300} />
+        <Image alt="logo" src='/socis/22.png' width={570} height={300} />  
         <Image alt="logo" src='/socis/21.png' width={570} height={300} />
         <Image alt="logo" src='/socis/17.png' width={570} height={300} />  
-        <Image alt="logo" src='/socis/22.png' width={570} height={300} />  
         <Image alt="logo" src='/socis/18.png' width={570} height={300} />    
         <Image alt="logo" src='/socis/19.png' width={570} height={300} />
         <Image alt="logo" src='/socis/23.png' width={570} height={300} />  
