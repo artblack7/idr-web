316890c32b994e8f8b4804653ea71ea7f523f3a3
artblack <stormeverrise@gmail.com>
2025-07-24 14:22:30 +0200

    catala home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,4 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import CatalanHome, { getStaticProps } from './[locale]/index';
 
-export default function IndexRedirect() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace('/ca'); // or your preferred default locale
-  }, [router]);
-  return null;
-} 
+export { getStaticProps };
+export default CatalanHome; 
