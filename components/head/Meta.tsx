acd7706dd2b60018d82ea763fa42e5e4d78a6ca9
artblack <stormeverrise@gmail.com>
2025-07-24 14:37:04 +0200

    favicon

--- components/head/Meta.tsx
+++ components/head/Meta.tsx
@@ -31,15 +31,10 @@ const Meta = (props: IMetaProps) => {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         {props.canonical && <link rel="canonical" href={props.canonical} key="canonical" />}
 
-        <link rel="apple-touch-icon" href={`${process.env.baseUrl}/apple-touch-icon.png`} key="apple" />
-        <link rel="icon" type="image/png" sizes="32x32" href={`${process.env.baseUrl}/favicon-32x32.png`} key="icon32" />
-        <link rel="icon" type="image/png" sizes="16x16" href={`${process.env.baseUrl}/favicon-16x16.png`} key="icon16" />
-        <link rel="icon" href={`${process.env.baseUrl}/favicon.ico`} key="favicon" />
-
-        <link rel="apple-touch-icon" sizes="180x180"  href="/favicon/apple-touch-icon.png" key="apple" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" key="apple" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" key="icon32" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" key="icon16" />
-        <link rel="icon" href="/favicon/favicon.ico" key="favicon"/>
         <link rel="manifest" href="/favicon/site.webmanifest" />
 
         {/* PAGE TITLE - using normal title attribute*/}
